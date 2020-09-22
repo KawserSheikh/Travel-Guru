605a7ba76f45e825d52add015ba447b40fa84341
KawserSheikh <hjdguli@gmail.com>
2020-09-23 00:05:06 +0300

    file added

---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBj-Exxbgh-1S5m-ojiZCQmnnbby13qMbs",
+    authDomain: "travel-guru-42d4d.firebaseapp.com",
+    databaseURL: "https://travel-guru-42d4d.firebaseio.com",
+    projectId: "travel-guru-42d4d",
+    storageBucket: "travel-guru-42d4d.appspot.com",
+    messagingSenderId: "688144552807",
+    appId: "1:688144552807:web:6088696ff81d4abcd315c2",
+    measurementId: "G-QYKGJ8F8NJ"
+};
+
+export default firebaseConfig;
