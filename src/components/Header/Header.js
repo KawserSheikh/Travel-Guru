e6e61421866b3c16cd49ad8a313e9591ca4f1bee
KawserSheikh <hjdguli@gmail.com>
2020-09-23 03:35:25 +0300

    completed deploy

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
                 <div className="collapse navbar-collapse " id="navbarNav">
                     <ul className="navbar-nav ">
                         <li className="nav-item active ">
-                            <Link className="nav-link " to="/">Destination</Link>
+                            <Link className="nav-link" to="/">Destination</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Blog</Link>
